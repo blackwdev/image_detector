2f280257feb85f3d14e4943c0ccf0e4e65f1880d
superbaby81230 <superbaby81230@gmail.com>
2023-06-02 11:36:17 +0300

    Detect dog and cat

---
+++ image.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8m.pt")
+
+results = model.predict("cat_dog.jpg")
+
+result = results[0]
+for box in result.boxes:
+  class_id = result.names[box.cls[0].item()]
+  cords = box.xyxy[0].tolist()
+  cords = [round(x) for x in cords]
+  conf = round(box.conf[0].item(), 2)
+  print("Object type:", class_id)
+  print("Coordinates:", cords)
+  print("Probability:", conf)
+  print("---")
